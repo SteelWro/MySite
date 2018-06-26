ca507ed3c6abd2804c5b2c7b1d811fd59d3f4847
STEEL\Stalowy <s5194@horyzont.eu>
2018-06-26 14:52:18 +0200

    add photos content and little clear code

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 <script
 src="https://code.jquery.com/jquery-3.1.1.min.js"
 integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-crossorigin="anonymous"></script>
-
+crossorigin="anonymous">
+</script>
 
 <script src="semantic/dist/semantic.min.js"></script>
+
+<script>
+    $('.toc.item').click(function(){
+      $('.ui.sidebar')
+    .sidebar('toggle');
+    });
+</script>
