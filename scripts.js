7106adb376abc536bd69dee0eaa0445129114e76
STEEL\Stalowy <s5194@horyzont.eu>
2018-06-27 13:31:28 +0200

    Made menu buttons active and no active JS, fixed sidebar, made auto scroll after clicked button

--- scripts.js
+++ scripts.js
@@ -4,11 +4,26 @@ integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
 crossorigin="anonymous">
 </script>
 
-<script src="semantic/dist/semantic.min.js"></script>
-
+<script src="semantic/dist/semantic.min.js"> </script>
 <script>
     $('.toc.item').click(function(){
       $('.ui.sidebar')
     .sidebar('toggle');
     });
 </script>
+
+<script>
+          $(document).ready(function(){
+              $('a[href^="#"]').on('click', function(event) {
+  
+                  var target = $( $(this).attr('href') );
+              
+                  if( target.length ) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                  }
+              });
+           });
+      </script>
