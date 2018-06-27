6614c8c9a566aecb85de4d1440aea4eac17f6676
STEEL\Stalowy <s5194@horyzont.eu>
2018-06-27 15:17:18 +0200

    changed color menu

--- scripts.js
+++ scripts.js
@@ -12,18 +12,4 @@ crossorigin="anonymous">
     });
 </script>
 
-<script>
-          $(document).ready(function(){
-              $('a[href^="#"]').on('click', function(event) {
-  
-                  var target = $( $(this).attr('href') );
-              
-                  if( target.length ) {
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                  }
-              });
-           });
-      </script>
+
